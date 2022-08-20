b75e1e5c4e7358c9b992b64bdb6f4f84a18527fc
Njeri Ngungu <janetnjerin89@gmail.com>
2022-08-20 15:08:20 +0300

    commit tree 3db4d54cf6dee2f640c0544db0c5828440f29009

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(match)
+        @match = match
+
+        matches = @match.select do |word|
+            if word.chars.sort == @word.chars.sort
+                word
+            end
+        end
+
+        matches
+
+    end
+
+
+end
+
